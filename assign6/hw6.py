0e2ab9689c1a619829a9cf91cccbc2bc8524a4d0
Jensen Dempsey <jdemp@jensendempsey.com>
2018-03-22 00:04:23 -0600

    p1

--- assign6/hw6.py
+++ assign6/hw6.py
@@ -2,21 +2,23 @@ import tensorflow as tf
 import edward as ed
 import numpy as np
 
-G1 = ed.models.Categorical(probs=[0.5,0.5])
+G1 = ed.models.Bernoulli(probs=.5)
 
-p_next = tf.where(tf.cast(G1,tf.bool),[0.9,0.1],[.1,.9])
+p_nextg2 = tf.where(tf.cast(G1,tf.bool),0.9,0.1)
+p_nextg3 = tf.where(tf.cast(G1,tf.bool),0.9,0.1)
 
-G2 = ed.models.Categorical(probs=p_next)
-G3 = ed.models.Categorical(probs=p_next)
+G2 = ed.models.Bernoulli(probs=p_nextg2)
+G3 = ed.models.Bernoulli(probs=p_nextg3)
 
-mean_x2 = tf.where(tf.cast(G2,tf.bool),50.,60.)
-mean_x3 = tf.where(tf.cast(G3,tf.bool),50.,60.)
+mean_x2 = tf.where(tf.cast(G2,tf.bool),60.,50.)
+mean_x3 = tf.where(tf.cast(G3,tf.bool),60.,50.)
 sig = np.float32(np.sqrt(10))
 X2 = ed.models.Normal(loc=mean_x2,scale=sig)
 X3 = ed.models.Normal(loc=mean_x3,scale=sig)
 
-g1_2 = ed.complete_conditional(G1)
+part1 = ed.models.Bernoulli(probs=tf.nn.sigmoid(tf.Variable(tf.random_normal([]))))
+ed.get_session()
+inf = ed.KLpq({G1:part1},data={X2:tf.constant(50,dtype=tf.float32)})
 
-sess = tf.Session()
-
-sess.run(g1_2,{X2:np.array([50],dtype=np.float64)})
+inf.run(n_samples=200)
+print(part1.probs.eval())
