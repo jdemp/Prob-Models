92611a83d5d09179e1f4731c473f3109131c4213
Jensen Dempsey <jdemp@jensendempsey.com>
2018-05-02 23:54:52 -0600

    final code

--- final/part1.py
+++ final/part1.py
@@ -13,11 +13,13 @@ block3 = multivariate_normal([0.5,0.6], uncertainty)
 block4 = multivariate_normal([-0.2,0.1], uncertainty)
 block5 = multivariate_normal([0.7,0.7], uncertainty)
 block6 = multivariate_normal([-0.4,0.6], uncertainty)
+block7 = multivariate_normal([0.4,0.2], uncertainty)
 
 robot_position = [0,0]
 robot_reach = 1
+position_of_agent = [5,3]
 
-blocks = [block1,block2,block3,block4,block5,block6]
+blocks = [block1,block2,block3,block4,block5,block6,block7]
 
 table_x_edges = [-.8,.8]
 table_y_edges = [.0,.7]
@@ -25,6 +27,7 @@ edge_buffer = .05
 edge_penalty = 20
 edge_weight = -5
 pdf_weight = -10
+agent_distance_weight = -0.5
 
 def dist(x1,x2):
     return np.sqrt((x1[0]-x2[0])**2 + (x1[1]-x2[1])**2)
@@ -52,33 +55,42 @@ def placement(x,y):
         pdf+=block.pdf([x,y])
     #penalty for nearness to edges
     edge = total_edge_penalty(x,y)
-    return edge_weight*edge+pdf_weight*pdf
+    distance_to_agent = dist([x,y],position_of_agent)
+    return edge_weight*edge + pdf_weight*pdf + agent_distance_weight*(distance_to_agent**2)
 
 def noisy_placement(x,y):
-    return placement(x, y) + np.random.normal(0,scale=1.0)
+    return placement(x, y) + np.random.normal(0,scale=2)
 def main():
     """Run the demo."""
     # grab a test function
-
-    table_corners=[-1,-1,1,1]
-
-    X = np.linspace(table_corners[2],table_corners[0],num=100)
-    Y = np.linspace(table_corners[1],table_corners[3],num=100)
-    F = np.zeros((100,100))
-    max_placement = (-100,3,3)
-    for x in range(len(X)):
-        for y in range(len(Y)):
-            F[y][x] = placement(X[x],Y[y])
-            if F[x][y]>max_placement[0]:
-                max_placement = (F[x][y],x,y)
-    plt.contourf(X,Y,F)
-    plt.show()
+    #
+    # table_corners=[-.8,0,.8,.7]
+    # #
+    # X = np.linspace(table_corners[0],table_corners[2],num=200)
+    # Y = np.linspace(table_corners[1],table_corners[3],num=200)
+    # max_value = (-100,10,10)
+    # for x in range(len(X)):
+    #     for y in range(len(Y)):
+    #         value = placement(X[x], Y[y])
+    #         if value>max_value[0]:
+    #             max_value = (value,X[x],Y[y])
+    # print(max_value)
+    # plt.contourf(X,Y,F)
+    # plt.colorbar()
+    # plt.show()
 
 
     boundaries=[-.8,0,.8,.7]
     bo = BayesianOptimization(noisy_placement, {'x':(boundaries[0],boundaries[2]),'y':(boundaries[1],boundaries[3])})
-    bo.maximize(init_points=10,n_iter=25,kappa=2.576)
+    bo.maximize(init_points=5,n_iter=1,kappa=5)
     print(bo.res['max'])
+    best = bo.res['max']
+    true_opt = (-13.1036,.7437,.3025)
+    while  dist([best['max_params']['x'],best['max_params']['y']],[true_opt[1],true_opt[2]])>.1:
+        bo.maximize(init_points=0,n_iter=1,kappa=5)
+        best = bo.res['max']
+        if best['max_val']>true_opt[0]:
+            print('found a value higher than true opt')
 
     # plot the final model
 
